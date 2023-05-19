47b63e4c40ecaa590c106661ceed25cd44213758
alperencngz <cengiz.ozturk@ozu.edu.tr>
2023-05-19 10:11:09 +0300

    Added a AddMovie form and functionality to send my movies to a FireShip database and read from there
    This was pretty much the basics of POST, GET etc.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,85 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
-
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const fetchMoviesHandler = useCallback(async () => {
-
     setIsLoading(true);
     setError(null);
-
     try {
-      const response = await fetch("https://react-http-e80c9-default-rtdb.firebaseio.com/movies.json");
-
+      // I created a realtime database on firebase for this http below
+      const response = await fetch('https://react-http-[FROMFIREBASE]-default-rtdb.firebaseio.com/movies.json');
       if (!response.ok) {
-        throw new Error(response.status);
+        throw new Error('Something went wrong!');
       }
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        };
-      });
-      setMovies(transformedMovies);
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-    // We would've added our external dependencies in the [] below
   }, []);
 
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
+  async function addMovieHandler(movie) {
+    // we could have done try catch here exactly like before fyi
+    const response = await fetch("https://react-http-[FROMFIREBASE]-default-rtdb.firebaseio.com/movies.json", {
+      method: "POST",
+      body: JSON.stringify(movie),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+  }
+
   let content = <p>Found no movies.</p>;
 
   if (movies.length > 0) {
-    content = <MoviesList movies={movies}/>;
+    content = <MoviesList movies={movies} />;
   }
 
   if (error) {
-    content = <p>{"Error code is: " + error}</p>
+    content = <p>{error}</p>;
   }
 
   if (isLoading) {
-    content = <p>Loading...</p>
+    content = <p>Loading...</p>;
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
-        {content}
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
